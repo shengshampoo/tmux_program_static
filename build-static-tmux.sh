27d1fad8897e1fd73c5134bcc5ea6f055022fabb
shengshampoo <shengzh@tutanota.com>
2025-09-12 11:42:47 +0000

    Update build-static-tmux.sh

--- build-static-tmux.sh
+++ build-static-tmux.sh
@@ -8,6 +8,7 @@ mkdir -p $WORKSPACE
 mkdir -p /work/artifact
 
 # libutempter
+cd $WORKSPACE
 git clone https://github.com/altlinux/libutempter
 cd libutempter/libutempter
 make
@@ -16,6 +17,7 @@ DESTDIR=/ make install
 cp libutempter.a /usr/lib/
 
 # tmux
+cd $WORKSPACE
 aa=$(curl -s "https://api.github.com/repos/tmux/tmux/releases/latest" | grep -Po '"tag_name": "\K[0-9a.]+')
 curl -sL $( curl -s "https://api.github.com/repos/tmux/tmux/releases/latest" | jq -r '.assets[] | select(.content_type == "application/x-gzip") | {browser_download_url}  | .browser_download_url ') | tar xv --gzip
 cd tmux-$aa
@@ -24,6 +26,6 @@ make
 make install
 
 cd /usr/local
-tar vcJf ./tmuxmm.tar.xz tmuxmm.tar.xz
+tar vcJf ./tmuxmm.tar.xz tmuxmm
 
 mv ./tmuxmm.tar.xz /work/artifact/
