531f5eac18b12b1bd3e2f9e0a1b349b658999837
shengshampoo <shengzh@tutanota.com>
2025-09-12 11:01:38 +0000

    Create build-static-tmux.sh

---
+++ build-static-tmux.sh
@@ -0,0 +1,21 @@
+
+#! /bin/bash
+
+set -e
+
+WORKSPACE=/tmp/workspace
+mkdir -p $WORKSPACE
+mkdir -p /work/artifact
+
+# tmux
+aa=$(curl -s "https://api.github.com/repos/tmux/tmux/releases/latest" | grep -Po '"tag_name": "\K[0-9a.]+')
+curl -sL $( curl -s "https://api.github.com/repos/tmux/tmux/releases/latest" | jq -r '.assets[] | select(.content_type == "application/x-gzip") | {browser_download_url}  | .browser_download_url ') | tar xv --gzip
+cd tmux-$aa
+CFLAGS="$CFLAGS -static" LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr/local/tmuxmm --enable-static --enable-utempter --enable-utf8proc --enable-jemalloc
+make
+make install
+
+cd /usr/local
+tar vcJf ./tmuxmm.tar.xz tmuxmm.tar.xz
+
+mv ./tmuxmm.tar.xz /work/artifact/
